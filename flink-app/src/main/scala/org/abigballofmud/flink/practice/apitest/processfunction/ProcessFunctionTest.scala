9de85ae3055eeef8c7a1d175134e03d38dc851d2
isacc <codingdebugallday@163.com>
2020-04-07 00:31:23 +0800

    [ADD] add ProcessFunctionTest

--- flink-app/src/main/scala/org/abigballofmud/flink/practice/apitest/processfunction/ProcessFunctionTest.scala
+++ flink-app/src/main/scala/org/abigballofmud/flink/practice/apitest/processfunction/ProcessFunctionTest.scala
@@ -1,7 +1,9 @@
 package org.abigballofmud.flink.practice.apitest.processfunction
 
 import org.abigballofmud.flink.practice.apitest.source.SensorReading
+import org.apache.flink.api.common.functions.RichFlatMapFunction
 import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
@@ -46,14 +48,83 @@ object ProcessFunctionTest {
       .keyBy(_.id)
       .process(new TempIncrAlert())
 
+    // 需求：温度骤变触发报警
+    val processedStream2: DataStream[(String, Double, Double)] = dataStream
+      .keyBy(_.id)
+      .flatMap(new TempChangeAlert(10.0))
+    //      .process(new TempChangeAlert2(10.0))
+
+    // 改写上面的flatMap
+    val processedStream3: DataStream[(String, Double, Double)] = dataStream
+      .keyBy(_.id)
+      .flatMapWithState[(String, Double, Double), Double] {
+        // 如果没有状态的话，也就是没有数据来过，那么就将当前数据温度值存入状态
+        case (input: SensorReading, None) => (List.empty, Some(input.temperature))
+        // 如果有状态，就应该与上次的温度值比较差值，如果大于阈值就输出报警
+        case (input: SensorReading, lastTemp: Some[Double]) =>
+          val diff: Double = input.temperature - lastTemp.get.abs
+          if (diff > 10.0) {
+            (List((input.id, lastTemp.get, input.temperature)), Some(input.temperature))
+          } else {
+            (List.empty, Some(input.temperature))
+          }
+      }
+
     dataStream.print("input data")
-    processedStream.print("alert data")
+    //    processedStream.print("incr data")
+    processedStream2.print("change data")
 
     env.execute("process function test")
   }
 
 }
 
+//noinspection DuplicatedCode
+class TempChangeAlert(threshold: Double) extends RichFlatMapFunction[SensorReading, (String, Double, Double)] {
+
+  // 定义一个状态，用来保存上一个数据的温度值
+  private var lastTempState: ValueState[Double] = _
+
+
+  override def open(parameters: Configuration): Unit = {
+    lastTempState = getRuntimeContext.getState(new ValueStateDescriptor[Double]("lastTemp", classOf[Double]))
+  }
+
+  override def flatMap(value: SensorReading, out: Collector[(String, Double, Double)]): Unit = {
+    // 先取出上一个温度值
+    val lastTemp: Double = lastTempState.value()
+    // 当前的温度值和上次的求差，如果大于阈值，输出报警信息
+    // 忽略第一条数据的bug
+    val diff: Double = (value.temperature - lastTemp).abs
+    if (diff > threshold) {
+      out.collect((value.id, lastTemp, value.temperature))
+    }
+    // 更新状态
+    lastTempState.update(value.temperature)
+  }
+}
+
+//noinspection DuplicatedCode
+class TempChangeAlert2(threshold: Double) extends KeyedProcessFunction[String, SensorReading, (String, Double, Double)] {
+
+  // 定义一个状态，用来保存上一个数据的温度值
+  lazy val lastTempState: ValueState[Double] = getRuntimeContext.getState(new ValueStateDescriptor[Double]("lastTemp", classOf[Double]))
+
+  override def processElement(value: SensorReading, ctx: KeyedProcessFunction[String, SensorReading, (String, Double, Double)]#Context, out: Collector[(String, Double, Double)]): Unit = {
+    // 先取出上一个温度值
+    val lastTemp: Double = lastTempState.value()
+    // 当前的温度值和上次的求差，如果大于阈值，输出报警信息
+    // 忽略第一条数据的bug
+    val diff: Double = (value.temperature - lastTemp).abs
+    if (diff > threshold) {
+      out.collect((value.id, lastTemp, value.temperature))
+    }
+    // 更新状态
+    lastTempState.update(value.temperature)
+  }
+}
+
+//noinspection DuplicatedCode
 class TempIncrAlert() extends KeyedProcessFunction[String, SensorReading, String] {
 
   // 定义一个状态，用来保存上一个数据的温度值
@@ -89,3 +160,4 @@ class TempIncrAlert() extends KeyedProcessFunction[String, SensorReading, String
   }
 
 }
+
